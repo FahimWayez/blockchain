f851feff4bdc7f948f61272a8b0470c60a734458
Fahim Wayez <fahimwayez1@gmail.com>
2024-01-31 05:02:34 +0600

    added new transaction method

--- blockchain/dev/blockchain.js
+++ blockchain/dev/blockchain.js
@@ -1,26 +1,43 @@
 function Blockchain() {
     this.chain = []; //shobgula block ja banabo ja mine hobe shob ekhane chain akare add hobe
-    this.newTransactions = []; //shob transaction hold hobe, even before egula block e dhukar age
+    this.pendingTransactions = []; //shob transaction hold hobe, even before egula block e dhukar age
 }
 
-//creating a new block
+//creating new block
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timeStamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce,
         hash: hash,
         previousBlockHash: previousBlockHash,
     };
 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
 
     return newBlock;
 }
 
+
+//getting the previous block in chain
 Blockchain.prototype.getLastBlock = function () {
     return this.chain[this.chain.length - 1];
+}
+
+
+
+//creating new transaction
+Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient,
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
 }
 module.exports = Blockchain;
