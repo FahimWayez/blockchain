7b41362d3c93cc70b8478b73a0db20f309e68eae
Fahim Wayez <fahimwayez1@gmail.com>
2024-01-31 05:14:36 +0600

    tested new transaction

--- blockchain/dev/test.js
+++ blockchain/dev/test.js
@@ -3,8 +3,11 @@ const Blockchain = require('./blockchain');
 const dclCoin = new Blockchain();
 
 dclCoin.createNewBlock(123, '0x0000', 'asdf1234');
-dclCoin.createNewBlock(342, 'asdf1234', 'poqw112');
-dclCoin.createNewBlock(876, 'poqw112', 'tyrgdf12');
-dclCoin.createNewBlock(100, 'tyrgdf12', '54dsfasa');
+dclCoin.createNewTransaction(100, 'FAHIMx32nj12', 'RIAN3wredfdd')
+
+dclCoin.createNewBlock(24234, 'asdf1234', 'uytre98uy');
+
+
 
 console.log('dclCoin', dclCoin);
+console.log('dclCoin', dclCoin.chain[1]); //transaction add hoise notun kore oita dekhano shudhu
