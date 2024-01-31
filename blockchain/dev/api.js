3317fbe0120cfb88e9803d54de1c88cf23de7409
Fahim Wayez <fahimwayez1@gmail.com>
2024-01-31 23:35:25 +0600

    transaction API added

--- blockchain/dev/api.js
+++ blockchain/dev/api.js
@@ -1,25 +1,30 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
 
-//when hitted, the entire blockchain will be sent
+//jodi ekta request ashe json data shoho postman theke tokhon parse kore nitesi
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//when hit, the entire blockchain will be sent
 app.get('/blockchain', function (req, res) {
 
 });
 
-
-
-//when hitted, it will create a new transaction in the blockchain
+//when hit, it will create a new transaction in the blockchain
 app.post('/transaction', function (req, res) {
-
+    console.log(req.body);
+    res.send(`The amount of the transaction is ${req.body.amount} dlcCoin.`);
 });
 
-
-//when hitted, it will mine or create a new block for us
+//when hit, it will mine or create a new block for us
 app.get('/mine', function (req, res) {
 
 });
 
+
+// app.use('/transaction', express);
 app.listen(3000, function () {
     console.log('Listening to port 3000...');
 })
